1f4418bd691cbcd128f88576102abfbda5b5e69a
ArtemDubovitskiy <artemdubovicki@yandex.ru>
2024-05-11 14:28:17 +0300

    Сверстан основной экран счетчика

--- CounterTestGit/ViewController.swift
+++ CounterTestGit/ViewController.swift
@@ -4,15 +4,72 @@
 //
 //  Created by Artem Dubovitsky on 11.05.2024.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    // MARK: - Private properties
+    private var count = 0
+    
+    // MARK: - UI-Elements
+    private lazy var counterLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Нажми кнопку)"
+        label.font = UIFont.systemFont(ofSize: 26, weight: .regular)
+        label.textColor = .black
+        label.backgroundColor = .clear
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private lazy var counterAddButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Увеличить значение", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        button.setTitleColor(.blue, for: .normal)
+        button.layer.cornerRadius = 16
+        button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        button.addTarget(self, action: #selector(didTabCounertButton), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        addSubviews()
+        setupConstrains()
     }
-
-
+    
+    // MARK: - Setup View
+    private func addSubviews() {
+        [counterLabel, counterAddButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
+    
+    private func setupConstrains() {
+        NSLayoutConstraint.activate([
+            counterLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            counterLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            counterLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            counterAddButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            counterAddButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            counterAddButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            counterAddButton.heightAnchor.constraint(equalToConstant: 75)
+        ])
+    }
+    
+    // MARK: - Private Methods
+    private func changeCount() {
+        count += 1
+        counterLabel.text = "Значение счетчика:\n\(count)"
+    }
+    
+    // MARK: - Actions
+    @objc private func didTabCounertButton() {
+        changeCount()
+    }
 }
