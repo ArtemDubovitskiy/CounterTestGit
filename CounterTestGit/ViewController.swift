90551ed45af33a4edb2e14310352bfa029eeb18a
ArtemDubovitskiy <artemdubovicki@yandex.ru>
2024-05-12 17:24:59 +0300

    Добавлен лейбл

--- CounterTestGit/ViewController.swift
+++ CounterTestGit/ViewController.swift
@@ -11,9 +11,20 @@ final class ViewController: UIViewController {
     private var count = 0
     
     // MARK: - UI-Elements
-    private lazy var counterLabel: UILabel = {
+    private lazy var label: UILabel = {
         let label = UILabel()
         label.text = "Нажми кнопку)"
+        label.font = UIFont.systemFont(ofSize: 26, weight: .regular)
+        label.textColor = .black
+        label.backgroundColor = .clear
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private lazy var counterLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
         label.font = UIFont.systemFont(ofSize: 26, weight: .regular)
         label.textColor = .black
         label.backgroundColor = .clear
@@ -43,7 +54,7 @@ final class ViewController: UIViewController {
     
     // MARK: - Setup View
     private func addSubviews() {
-        [counterLabel, counterAddButton].forEach {
+        [label, counterLabel, counterAddButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview($0)
         }
@@ -51,7 +62,11 @@ final class ViewController: UIViewController {
     
     private func setupConstrains() {
         NSLayoutConstraint.activate([
-            counterLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            counterLabel.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 50),
             counterLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             counterLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             
